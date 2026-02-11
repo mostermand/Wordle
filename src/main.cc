c7a5816146e6403e96310e650ce757b2dc84418f
mostermand <mostermand@gmail.com>
2026-02-11 12:04:46 +0100

    Move event loop to separate function

--- src/main.cc
+++ src/main.cc
@@ -9,9 +9,6 @@
 
 #include "dictionary.h"
 
-
-
-
 int main(int argc, char *argv[]) {
     std::ifstream wordle_ord("data/wordle_ord.txt");
     std::vector<std::string> dict;
@@ -22,5 +19,16 @@ int main(int argc, char *argv[]) {
     //Initialize dictionary from wordlist file and pick a random solution
     readDict(wordle_ord, dict);
     solution = dict[rand()%dict.size()];
+    while(true) {
+        std::string line;
+
+        std::cout<<">";
+        std::getline(std::cin, line);
+        if(inDict(line)) {
+
+        } else {
+            std::cout<<"Word is not in dictionary"
+        }
+    }
     
 }
