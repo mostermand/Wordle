034c2435dbb65472e5f39369dd905c63b30a1f90
mostermand <mostermand@gmail.com>
2026-02-19 14:37:39 +0100

    Remove redundant function and tests

--- src/dictionary.cc
+++ src/dictionary.cc
@@ -19,12 +19,3 @@ void readDict(std::istream& dictFile, std::vector<std::string>& dictOut) {
 bool inDict(std::string word, const std::vector<std::string>& dict) {
     return std::find(dict.begin(), dict.end(), word) != dict.end();
 }
-
-void matchWord(std::string candidate, std::string solution, std::vector<std::pair<bool, bool> >& matchOut) {
-    bool posMatch, genMatch; // posMatch: letter matched exact position; genMatch: letter is in word.
-    for(int i = 0; i < candidate.size() && i < solution.size(); ++i) {
-        posMatch = candidate[i] == solution[i];
-        genMatch = solution.find(candidate[i]) != std::string::npos;
-        matchOut.push_back(std::make_pair(posMatch, genMatch));
-    }
-}
--- src/dictionary.h
+++ src/dictionary.h
@@ -12,8 +12,4 @@ void readDict(std::istream& dictFile, std::vector<std::string>& dictOut);
 
 //Checks if word is in dict
 bool inDict(std::string word, const std::vector<std::string>& dict);
-
-//Writes a vector of (bool,bool) for each letter in candidate: first bool is whether there is a positional match,
-//second is if letter is in solution somewhere.
-void matchWord(std::string candidate, std::string solution, std::vector<std::pair<bool, bool> >& matchOut);
 #endif
